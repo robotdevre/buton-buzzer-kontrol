2e687dc5c6afc8b5bf2c5871c019fbe58a864510
Uğur Kerim Sirke <robotdevre@gmail.com>
2025-04-07 15:46:55 +0300

    Update buton-buzzer-kontrol.ino

--- buton-buzzer-kontrol.ino
+++ buton-buzzer-kontrol.ino
@@ -2,6 +2,10 @@ const int buzzerPin = 8;
 const int buttonPin = 7;
 
 void setup() {
+  Serial.begin(9600);
+  delay(100); // Bazı durumlarda gerekli
+  Serial.println("GITHUB TEST");
+  
   pinMode(buzzerPin, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);  // Dahili pull-up direnci etkinleştirildi
 }
